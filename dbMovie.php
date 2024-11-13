e498d3b17f30fc707fd6b11b16bd26bfe6065ccc
2237880 <rojinakhatri792@gmail.com>
2024-11-14 03:28:23 +0545

    Added Auth and File Spearation

---
+++ dbMovie.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+
+session_start();
+$host = 'mi-linux.wlv.ac.uk'; // Database host
+$username = '2237880'; // Database username
+$password = 'Qwerty1234@@'; // Database password
+$database = 'db2237880'; // Database name
+
+
+// $server = "localhost";
+// $username = "db2237880";
+// $password = "qwerty12@@";
+// $database = "shop";
+
+
+
+$conn = new mysqli($host, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
