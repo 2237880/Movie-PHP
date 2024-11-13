8dad0980e8a959f396154d2398708f05f726f052
2237880 <rojinakhatri792@gmail.com>
2024-11-14 00:35:00 +0545

    DB setup Completed

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+
+session_start();
+$host = 'localhost'; // Database host
+$username = 'root'; // Database username
+$password = ''; // Database password
+$database = 'Moviesdb'; // Database name
+
+
+
+$conn = new mysqli($host, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
